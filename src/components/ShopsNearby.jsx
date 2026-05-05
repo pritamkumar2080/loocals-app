cb1053a104a6d5d04a342111b1f036ceac7a86c8
pritamkuamr2080 <pritamkumar2080@gmail.com>
2026-05-05 23:52:10 +0530

    fixse searchnearby case issue

--- src/components/ShopsNearby.jsx
+++ src/components/ShopsNearby.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ShopCard from "./ShopCard";
-import { shops } from "../data/shop";
+import { shops } from "../data/shops";
 
 const ShopsNearby = () => {
   const navigate = useNavigate();
