1e3067fda49d0533994095d92dbe9e16b4186d28
pritamkuamr2080 <pritamkumar2080@gmail.com>
2026-05-06 00:00:42 +0530

    fixed timeout2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % slides.length);
-    }, 2000);
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
